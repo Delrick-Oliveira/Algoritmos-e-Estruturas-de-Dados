0de044d728abdef48ed8d6c9f3d25e6efa61de50
Delrick Oliveira <delricknunes@gmail.com>
2018-08-22 19:20:01 +0000

    update

--- functions/sequentialSearch.cpp
+++ functions/sequentialSearch.cpp
@@ -1,2 +1,15 @@
-#include "Person.h"
-#include <iostream>
+#include "sequentialSearch.h"
+
+template<class Type1, class Type2>
+sequentialSearch(Type1 key, vector<Type2> list, int tam)
+{
+    int i;
+    for(i=0; i<tam;i++)
+    {
+        if(key==list[i].key)
+        {
+            return i;
+        }
+    }
+    return null;
+}
